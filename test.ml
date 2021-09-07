8788e43cfc5031444c0b7a37dc28b842549ea374
Jack Feser <feser@csail.mit.edu>
2021-09-07 16:54:41 -0400

    Initial commit

---
+++ test.ml
@@ -0,0 +1,36 @@
+let f x = 0
+
+type ext = ..
+
+module Test : sig type s val yyy : s end = struct
+  let xxx = 0
+    type t = int
+type s = A | B | C
+           let yyy = B
+type ext += Test
+
+module Unused = struct
+  end
+end
+    
+let g = 1
+
+module Test1 = struct
+  open Test
+let yyy = 0
+end
+
+let unused_for =
+  for i = 0 to 10 do
+    ()
+  done
+
+class parent = object
+end
+
+class child = object
+  inherit parent as super
+end
+
+let rec non_rec x = x + 1
+
